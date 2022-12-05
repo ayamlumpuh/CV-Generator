80bb73ba0da0a0f79814455fdcad4330cc73d33b
Asyam Fadhilah Akbar <asyam.akbar@yahoo.com>
2022-12-05 21:40:58 +0700

    Update json

--- src/CVServiceProvider.php
+++ src/CVServiceProvider.php
@@ -9,8 +9,9 @@ class CVServiceProvider extends ServiceProvider {
     public function boot() {
         $this -> loadRoutesFrom(__DIR__."/routes/web.php");
         $this -> loadViewsFrom(__DIR__."/views","cv");
-        $this -> publishes([__DIR__. "/public"=>public_path('cvreq'),
-        __DIR__. "/views" =>resource_path('views/cvview')    
+        $this -> publishes([
+        __DIR__. "/public"=>public_path('cvreq'),
+        __DIR__. "/views" =>resource_path('views/cvview')
     ],["laravel-assets"]);
     }
     public function register() {
